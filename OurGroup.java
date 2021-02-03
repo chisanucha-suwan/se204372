fad7d9a378c7d64fc0c44c9fceb55db385d57955
KForster99 <krisda_@windowslive.com>
2021-02-03 21:17:24 +0700

    third

--- OurGroup.java
+++ OurGroup.java
@@ -8,6 +8,7 @@ public class OurGroup {
         groupMembers = new Vector<String>();
         groupMembers.add("Chisanucha"); //PUT YOUR NAME HERE
         groupMembers.add("Suckchai"); //PUT YOUR NAME HERE
+        groupMembers.add("Krisda"); //PUT YOUR NAME HERE
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
